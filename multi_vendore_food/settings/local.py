424dca43dc30e29f1dca8a230398fd6bcc12dba2
Mhadi Ahmed <mhadiahmed63@gmail.com>
2022-09-03 01:19:18 +0300

    add multibale settings folders

---
+++ multi_vendore_food/settings/local.py
@@ -0,0 +1,10 @@
+from email.policy import default
+from .base import *
+from decouple import config
+INSTALLED_APPS += ["debug_toolbar"]
+
+
+DEBUG=config("DEBUG", default=True, cast=bool)
+
+MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
