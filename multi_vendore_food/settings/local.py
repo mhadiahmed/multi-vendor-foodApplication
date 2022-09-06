002f884871b08bc48341d039c35ef09dabd2d20f
Mhadi Ahmed <mhadiahmed63@gmail.com>
2022-09-06 15:05:35 +0300

    add django debug tool bar to the front

--- multi_vendore_food/settings/local.py
+++ multi_vendore_food/settings/local.py
@@ -3,8 +3,13 @@ from .base import *
 from decouple import config
 INSTALLED_APPS += ["debug_toolbar"]
 
-
 DEBUG=config("DEBUG", default=True, cast=bool)
 
 MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
